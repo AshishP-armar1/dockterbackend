cb356facb369554bfb82aa8d633cdbd12f3c6821
AshishP-armar1 <atp4465@gmail.com>
2025-08-21 22:22:18 +0530

    Update db.py

--- core/db.py
+++ core/db.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-engine = create_engine(os.getenv("DATABASE_URL"), connect_args={"check_same_thread": False})
+engine = create_engine("sqlite:///./test.db", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
